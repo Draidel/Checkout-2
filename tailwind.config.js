c50b5aeebc5d21265fd426c2c659bb93fcf65779
Ariel <ariel@MacBook-Pro-de-Ariel.local>
2020-09-04 09:28:26 -0300

    Fix Fonts & Try to Fix Header Size

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     extend: {
       fontFamily: {
         'catamaran': ['Catamaran', 'Helvetica', 'sans-serif']
+      },
+      colors: {
+        'happy-dark-grey': '#282828',
+        'happy-light-grey': '#707070',
+      },
+      fontSize: {
+        'happy-h1': '2.7rem',
       }
     },
   },
